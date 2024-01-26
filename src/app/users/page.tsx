285113e717be21b9b7bb75cd3ab21cefb0d52106
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-01-26 19:36:44 +0100

    Updatye

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import React from 'react'
 import PageTitle from '@/components/PageTitle'
 
-
-
 export default function UsersPage() {
   return (
-    <div className='flex-1  p-5 '>
+    <div className='flex-1 p-5 '>
       <div className='flex flex-col bg-white p-5 rounded-md'>
         <div className='flex items-center justify-between'>
           <PageTitle className='text-sm  py-3'>
